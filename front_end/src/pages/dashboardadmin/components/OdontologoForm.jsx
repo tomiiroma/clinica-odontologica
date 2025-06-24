b1342a8eef97610a0fac6ef6c1c45c5d16a922e5
att124 <daniel.fernandez@davinci.edu.ar>
2025-06-24 16:26:37 -0300

    Solucionado error al modificar datos del odontologo

--- front_end/src/pages/dashboardadmin/components/OdontologoForm.jsx
+++ front_end/src/pages/dashboardadmin/components/OdontologoForm.jsx
@@ -22,25 +22,36 @@ export default function OdontologoForm({ modo, odontologo, onClose }) {
   }
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const method = modo === "editar" ? "PUT" : "POST"
-    const url = modo === "editar"
-      ? `http://localhost:3000/api/odontologos/${odontologo.id_odontologo}`
-      : "http://localhost:3000/api/odontologos"
+  e.preventDefault()
 
-    const res = await fetch(url, {
-      method,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form)
-    })
+  const method = modo === "editar" ? "PUT" : "POST"
+  const url = modo === "editar"
+    ? `http://localhost:3000/api/odontologos/${odontologo.id_odontologo}`
+    : "http://localhost:3000/api/odontologos"
 
-    if (res.ok) {
-      alert("Guardado correctamente")
-      onClose()
-    } else {
-      alert("Error al guardar")
-    }
+ 
+  const cleanForm = {
+    nombre: form.nombre,
+    apellido: form.apellido,
+    email: form.email,
+    telefono: form.telefono,
+    contraseña: form.contraseña
   }
+
+  const res = await fetch(url, {
+    method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cleanForm)
+  })
+
+  if (res.ok) {
+    alert("Guardado correctamente")
+    onClose()
+  } else {
+    alert("Error al guardar")
+  }
+}
+
 
   return (
     <form className="odontologo-form" onSubmit={handleSubmit}>
